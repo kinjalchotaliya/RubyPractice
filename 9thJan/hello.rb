a42ec3860f4f1113e993206257394aa645bee5e6
kinjalchotaliya <kinjal.chotaliya@bacancytechnology.com>
2017-01-09 19:16:30 +0530

    9th january ruby excercise

---
+++ 9thJan/hello.rb
@@ -0,0 +1,27 @@
+puts 'hello'
+#anything written after '#' is comment.
+
+=begin
+"Multiline
+comment"
+=end
+
+puts 'after multiline comment'
+5.times{print 'abc'}
+
+puts "hi #hello.."
+
+
+#math operations
+puts 1+2+34%10
+puts "math operation #{3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6}"
+
+#Relational operator
+
+puts "2 is less than -2: #{2 < -2}"
+puts "2+4 is greater or equal to -6+3: #{2+4 >= -6+3}"
+
+puts 3.0-2.89
+
+
+
