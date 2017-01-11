1e25a1944a569e860f7e6900ddb675b7c9a22a1e
kinjalchotaliya <kinjal.chotaliya@bacancytechnology.com>
2017-01-11 19:28:49 +0530

    11th jan

---
+++ 11thJan/escapeseq.rb
@@ -0,0 +1,34 @@
+
+puts "hello\nruby"
+puts "hello\truby"
+
+puts "\"she is 6'5\".."	#6'5"
+puts "it was 7\'5"	# 7'5
+
+
+puts "go for '\\'"  	#  '\'
+
+puts 'hey\t'		#print AS it is
+
+a=20
+puts """
+
+daccau\tdhbfai;we
+asjodiwqjod
+
+a=#{a}
+wqedijewo
+wqdmwod
+hello
+
+"""
+
+puts "hello\fworld"
+
+puts "roirjgje\rworld"  #  worldgje removes chars from LHS as no of char after '\r' and place remaining chars after RHS
+
+puts "sdjxsakdahs\vhello" #vertical tab
+
+puts "12\o20"
+
+puts "\x61" #a
