a49a00a8decf5b8af28c7d8450355dd6be142c08
kinjalchotaliya <kinjal.chotaliya@bacancytechnology.com>
2017-01-18 12:22:17 +0530

    Done

--- 17thJan/prog5.rb
+++ 17thJan/prog5.rb
@@ -1,13 +1,6 @@
-
 puts "Enter any number:"
 n=gets.chomp.to_i
-flag=0
-
-for i in 2..(n-1)
-
-	flag=1 if (n%i==0)
-
-end
-
-puts "#{n}=Prime" if flag==0 		
-puts "#{n}=Non Prime" if flag==1
+require 'prime'
+flag=Prime.instance.prime?(n)
+puts "#{n}=Prime" if flag==true
+puts "#{n}=Non Prime" if flag==false
