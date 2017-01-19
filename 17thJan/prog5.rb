26ce41e2ccfaccad92bd0b15b9fe8e39d2b3f8a1
kinjalchotaliya <kinjal.chotaliya@bacancytechnology.com>
2017-01-19 18:45:17 +0530

    prime

--- 17thJan/prog5.rb
+++ 17thJan/prog5.rb
@@ -2,12 +2,16 @@
 puts "Enter any number:"
 n=gets.chomp.to_i
 flag=0
-
-for i in 2..(n-1)
+start = Time.now
+for i in 2..n/2
 
 	flag=1 if (n%i==0)
 
 end
+endt = Time.now
+
+diff = endt - start
 
 puts "#{n}=Prime" if flag==0 		
 puts "#{n}=Non Prime" if flag==1
+puts diff
